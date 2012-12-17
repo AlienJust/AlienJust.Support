021673d5f907e37381dfdc1385f5d72d50363e3a
AlienJust <alienjustmail@gmail.com>
2012-12-17 14:12:24 +0600

    Изменения интерфейса IWindowSystem

--- AlienJust.Support.UserInterface.Contracts/IWindowSystem.cs
+++ AlienJust.Support.UserInterface.Contracts/IWindowSystem.cs
@@ -3,5 +3,7 @@ namespace AlienJust.Support.UserInterface.Contracts
 	public interface IWindowSystem
 	{
 		string ShowOpenFileDialog(string dialogTitle, string filter);
+		string ShowSaveFileDialog(string dialogTitle, string filter);
+		void ShowMessageBox(string message, string caption);
 	}
 }

e96654725dbbba4ed9cb4596795e8140db6e06fa
AJ01 <AJ001@mail.ru>
2013-07-10 18:09:58 +0600

    Добавлен контейнер для кросспоточного доступа к зачению переменной

---
+++ AlienJust.Support.Concurrent/SyncObjectContainer.cs
@@ -0,0 +1,31 @@
+﻿namespace AlienJust.Support.Concurrent {
+	/// <summary>
+	/// Хранит значение переменной, доступ к которому осуществляется синхронизованно
+	/// </summary>
+	/// <typeparam name="T">Тип значения</typeparam>
+	public sealed class SyncObjectContainer<T> {
+		private readonly object _sync;
+		private T _value;
+
+		public SyncObjectContainer(T initialValue) {
+			_sync = new object();
+			_value = initialValue;
+		}
+
+		public T Value {
+			// NOTE: Thread safe through monitor
+			get {
+				T result;
+				lock (_sync) {
+					result = _value;
+				}
+				return result;
+			}
+			set {
+				lock (_sync) {
+					_value = value;
+				}
+			}
+		}
+	}
+}

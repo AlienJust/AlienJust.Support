04dfd2e544683bef5faedb5faacbc7b58a92bb87
AJ01 <AJ001@mail.ru>
2013-12-09 18:02:47 +0600

    Добавил новый проект, в котором будут собраны вспомогательные методы для работы со временем

---
+++ AlienJust.Support.Time/TimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AlienJust.Support.Time {
+	public static class TimeExtensions {
+		public static DateTime RoundToLatestHalfAnHour(this DateTime time) {
+			var minute = time.Minute > 30 ? 30 : 0;
+			return new DateTime(time.Year, time.Month, time.Day, time.Hour, minute, 0);
+		}
+	}
+}

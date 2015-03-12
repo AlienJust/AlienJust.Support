e601722148511bb1d8c436749c3281db573a019d
AJ01 <AJ001@mail.ru>
2015-03-12 10:31:31 +0500

    UserInterface.Contracts: updated IWindowSystem interface, added method for yes/no dialog showing

--- AlienJust.Support.UserInterface.Contracts/IWindowSystem.cs
+++ AlienJust.Support.UserInterface.Contracts/IWindowSystem.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace AlienJust.Support.UserInterface.Contracts
+﻿namespace AlienJust.Support.UserInterface.Contracts
 {
 	public interface IWindowSystem {
 		string ShowOpenFileDialog(string dialogTitle, string filter);
 		string ShowSaveFileDialog(string dialogTitle, string filter);
 		void ShowMessageBox(string message, string caption);
+		BinaryChoise ShowYesNoDialog(string message, string caption);
+	}
+
+	public enum BinaryChoise {
+		Yes,
+		No
 	}
 }

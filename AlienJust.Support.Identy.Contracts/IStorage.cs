b09f6cbe8c40f49df5cc3f619d5a7ed647ec989b
AJ01 <AJ001@mail.ru>
2015-09-15 16:08:16 +0500

    Added interface IStorage<>

---
+++ AlienJust.Support.Identy.Contracts/IStorage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AlienJust.Support.Identy.Contracts {
+	public interface IStorage<TDataItem> where TDataItem : IObjectWithIdentifier
+	{
+		IEnumerable<TDataItem> StoredItems { get; }
+		void Add(TDataItem item);
+		void Remove(TDataItem item);
+		void Update(TDataItem item);
+	}
+}

78995015033fe0027beebe49dddac89375ad64c1
AJ01 <AJ001@mail.ru>
2016-03-31 15:43:52 +0500

    Improved composition contracts Added simple composition release

--- AlienJust.Support.Composition.Contracts/ICompositionPart.cs
+++ AlienJust.Support.Composition.Contracts/ICompositionPart.cs
@@ -1,6 +1,12 @@
 namespace AlienJust.Support.Composition.Contracts {
-	public interface ICompositionPart {
+	public interface ICompositionPart : IUnknownLike {
 		string Name { get; }
 		void SetCompositionRoot(ICompositionRoot root);
+	}
+
+	public interface IUnknownLike {
+		void Release();
+		void AddRef();
+		int RefsCount { get; }
 	}
 }

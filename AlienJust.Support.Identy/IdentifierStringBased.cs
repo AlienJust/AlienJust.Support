4995527a51d899dc54f968a8b9332b49abbab966
AJ01 <AJ001@mail.ru>
2016-04-04 08:29:48 +0500

    Added id based on string.ToLower()

--- AlienJust.Support.Identy/IdentifierStringBased.cs
+++ AlienJust.Support.Identy/IdentifierStringBased.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using AlienJust.Support.Identy.Contracts;
 
 namespace AlienJust.Support.Identy {
@@ -16,6 +17,21 @@ namespace AlienJust.Support.Identy {
 
 		public override string ToString()
 		{
+			return _identyString;
+		}
+	}
+
+	public sealed class IdentifierStringToLowerBased : IIdentifier {
+		private readonly string _identyString;
+		public IdentifierStringToLowerBased(string identyString) {
+			_identyString = identyString.ToLower(CultureInfo.InvariantCulture);
+		}
+
+		public string IdentyString {
+			get { return _identyString; }
+		}
+
+		public override string ToString() {
 			return _identyString;
 		}
 	}

af01496dd0a3aae29692d46603853bde28b7f604
AJ01 <AJ001@mail.ru>
2016-10-24 15:24:03 +0500

    Misc changes

--- AlienJust.Support.Numeric/Crc16.cs
+++ AlienJust.Support.Numeric/Crc16.cs
@@ -1,28 +1,20 @@
 ﻿namespace AlienJust.Support.Numeric {
-	public struct Crc16
-	{
-		private readonly byte _low;
-		private readonly byte _high;
+    public struct Crc16 {
+        private readonly byte _low;
+        private readonly byte _high;
 
-		public Crc16(byte low, byte high)
-			: this()
-		{
-			_low = low;
-			_high = high;
-		}
+        public Crc16(byte low, byte high)
+            : this() {
+            _low = low;
+            _high = high;
+        }
 
-		public byte Low
-		{
-			get { return _low; }
-		}
+        public byte Low => _low;
 
-		public byte High
-		{
-			get { return _high; }
-		}
+        public byte High => _high;
 
-		public override string ToString() {
-			return "L: 0x" + _low.ToString("X2") + ", H: 0x" + _high.ToString("X2");
-		}
-	}
+        public override string ToString() {
+            return "L: 0x" + _low.ToString("X2") + ", H: 0x" + _high.ToString("X2");
+        }
+    }
 }

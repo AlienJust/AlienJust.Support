baa9a69cda0c2ff010c2df6ab20ddc42ef7782b8
AJ01 <AJ001@mail.ru>
2017-08-01 14:10:44 +0500

    Added SetBit and ResetBit methods as integer extensions

--- AlienJust.Support.Numeric/Bits/BitsExtensions.cs
+++ AlienJust.Support.Numeric/Bits/BitsExtensions.cs
@@ -68,35 +68,36 @@
 
 
 		public static byte ResetBit(this byte b, int bitNumber) {
+
 			return (byte)(b & ~(1 << bitNumber));
 		}
 
 		public static sbyte ResetBit(this sbyte b, int bitNumber) {
-			return (sbyte)(b | ~(1 << bitNumber));
+			return (sbyte)(b & ~(1 << bitNumber));
 		}
 
 		public static ushort ResetBit(this ushort b, int bitNumber) {
-			return (ushort)(b | ~(1 << bitNumber));
+			return (ushort)(b & ~(1 << bitNumber));
 		}
 
 		public static short ResetBit(this short b, int bitNumber) {
-			return (short)(b | ~(1 << bitNumber));
+			return (short)(b & ~(1 << bitNumber));
 		}
 
 		public static uint ResetBit(this uint b, int bitNumber) {
-			return b | ~((uint)1 << bitNumber);
+			return b & ~((uint)1 << bitNumber);
 		}
 
 		public static int ResetBit(this int b, int bitNumber) {
-			return b | ~(1 << bitNumber);
+			return b & ~(1 << bitNumber);
 		}
 
 		public static long ResetBit(this long b, int bitNumber) {
-			return b | ~((long)1 << bitNumber);
+			return b & ~((long)1 << bitNumber);
 		}
 
 		public static ulong ResetBit(this ulong b, int bitNumber) {
-			return b | ~((ulong)1 << bitNumber);
+			return b & ~((ulong)1 << bitNumber);
 		}
 	}
 

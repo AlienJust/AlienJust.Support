ec1d4147a2d907db454ffd8314b9b3b506d9cecf
AJ01 <AJ001@mail.ru>
2018-02-16 11:40:48 +0500

    Code comments

--- AlienJust.Support.Conversion.Contracts/IBuilderOneToOne.cs
+++ AlienJust.Support.Conversion.Contracts/IBuilderOneToOne.cs
@@ -1,5 +1,10 @@
 ﻿namespace AlienJust.Support.Conversion.Contracts
 {
+	/// <summary>
+	/// Converts single source objects to result one
+	/// </summary>
+	/// <typeparam name="TSource">Source object type</typeparam>
+	/// <typeparam name="TResult">Result object type</typeparam>
 	public interface IBuilderOneToOne<in TSource, out TResult> {
 		TResult Build(TSource source);
 	}

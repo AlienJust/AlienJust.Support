f49e90170c6f96156045f0a5e2f4d9f5e39a6c6b
AJ01 <AJ001@mail.ru>
2018-02-16 11:56:25 +0500

    Added .Conversion project

---
+++ AlienJust.Support.Conversion/RawAndConvertedValues.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AlienJust.Support.Conversion.Contracts;
+
+namespace AlienJust.Support.Conversion
+{
+	public sealed class RawAndConvertedValues<TRaw, TConverted> {
+		private readonly IBuilderOneToOne<TRaw, TConverted> _builder;
+		public TRaw RawValue { get; }
+		public RawAndConvertedValues(TRaw rawValue, IBuilderOneToOne<TRaw, TConverted> builder) {
+			_builder = builder;
+			RawValue = rawValue;
+		}
+		public TConverted ConvertedValue => _builder.Build(RawValue);
+	}
+}

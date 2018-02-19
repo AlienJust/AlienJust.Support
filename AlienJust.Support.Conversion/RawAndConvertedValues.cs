3116669a2c27d7ddc7403b65ee2442c33f14305a
AJ01 <AJ001@mail.ru>
2018-02-19 16:19:40 +0500

    Code cleanup

--- AlienJust.Support.Conversion/RawAndConvertedValues.cs
+++ AlienJust.Support.Conversion/RawAndConvertedValues.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using AlienJust.Support.Conversion.Contracts;
+﻿using AlienJust.Support.Conversion.Contracts;
 
 namespace AlienJust.Support.Conversion
 {

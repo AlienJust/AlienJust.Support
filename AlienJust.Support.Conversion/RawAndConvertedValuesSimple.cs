a2a1b0414f89d7c3ec47bc4f85476f61f1f69678
AJ01 <AJ001@mail.ru>
2018-03-16 14:25:43 +0500

    Added simplest IRawAndConvertedValues implementation

---
+++ AlienJust.Support.Conversion/RawAndConvertedValuesSimple.cs
@@ -0,0 +1,20 @@
+﻿using AlienJust.Support.Conversion.Contracts;
+
+namespace AlienJust.Support.Conversion
+{
+	public sealed class RawAndConvertedValuesSimple<TRaw, TConverted> : IRawAndConvertedValues<TRaw, TConverted>
+	{
+		public TRaw RawValue { get; }
+		public TConverted ConvertedValue { get; }
+
+		public RawAndConvertedValuesSimple(TRaw rawValue, TConverted convertedValue)
+		{
+			RawValue = rawValue;
+			ConvertedValue = convertedValue;
+		}
+
+		public override string ToString() {
+			return RawValue + " - " + ConvertedValue;
+		}
+	}
+}

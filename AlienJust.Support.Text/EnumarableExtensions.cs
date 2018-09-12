4fe8357086850ed052429db85800c0e96c027a5b
AJ01 <AJ001@mail.ru>
2018-09-12 16:59:22 +0500

    More extension methods for bytes collections

--- AlienJust.Support.Text/EnumarableExtensions.cs
+++ AlienJust.Support.Text/EnumarableExtensions.cs
@@ -12,5 +12,32 @@ namespace AlienJust.Support.Text {
 			result += "[" + count + "]";
 			return result;
 		}
+
+		public static string ToHexadecimalTextWithoutLength(this IEnumerable<byte> data) {
+			string result = string.Empty;
+			foreach (byte b in data) {
+				result += b.ToString("X2") + " ";
+			}
+			return result;
+		}
+
+		public static string ToTextDecimalWithLength(this IEnumerable<byte> data) {
+			int count = 0;
+			string result = string.Empty;
+			foreach (byte b in data) {
+				result += b + " ";
+				count++;
+			}
+			result += "[" + count + "]";
+			return result;
+		}
+
+		public static string ToTextDecimalWithoutLength(this IEnumerable<byte> data) {
+			string result = string.Empty;
+			foreach (byte b in data) {
+				result += b + " ";
+			}
+			return result;
+		}
 	}
 }
